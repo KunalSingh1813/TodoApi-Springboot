23f9d174192dec49be3afcfe2755588309676be8
Kunal Singh <singh.kunal1813@gmail.com>
2024-04-13 15:05:27 +0530

    dependency injection

--- src/main/java/com/example/TodoApiSpring/TodoController.java
+++ src/main/java/com/example/TodoApiSpring/TodoController.java
@@ -1,5 +1,7 @@
 package com.example.TodoApiSpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +12,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/todos")
 public class TodoController {
+
+
+    private TodoService todoService; //anotherTodoService     this is called composition
+
+    private TodoService todoService2; //fakeTodoService
     private static List<Todo> todoList;
 
     //Error message
     private static final String TODO_NOT_FOUND = "Todo not found";
-    public TodoController(){
+    public TodoController(
+            @Qualifier("anotherTodoService") TodoService todoService,
+            @Qualifier("fakeTodoService") TodoService todoService2){
+            this.todoService = todoService;
+            this.todoService = todoService2;
         todoList = new ArrayList<>();
         todoList.add(new Todo(1,false,"todo 1",1));
         todoList.add(new Todo(2,true,"todo 2",2));
@@ -22,7 +33,7 @@ public class TodoController {
 
   @GetMapping
   public ResponseEntity<List<Todo>> getTodos(@RequestParam(required = false, defaultValue = "true") Boolean isCompleted){
-        System.out.println("incoming query params: "+ isCompleted);
+        System.out.println("incoming query params: "+ isCompleted + " " + this.todoService.doSomething());
         return ResponseEntity.ok(todoList);
   }
 /*
